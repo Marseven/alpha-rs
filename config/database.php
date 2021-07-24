9077ad1f0c9a83f1cc08a047367c01ebfd5ef82d
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-07-24 16:32:24 +0100

    dbg 2

--- config/database.php
+++ config/database.php
@@ -44,12 +44,12 @@ return [
         ],
 
         'mysql' => [
-            'driver'    => 'mysql',
-            'host'      => '127.0.0.1',
-            'port'      => '8889',
-            'database'  => 'alpha',
-            'username'  => 'marseven',
-            'password'  => '27478433',
+            'driver'    => env('DB_CONNECTION'),
+            'host'      => env('DB_HOST'),
+            'port'      => env('DB_PORT'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
