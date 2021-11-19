930cc583343a8b7599fe540461addf976a130af3
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-11-19 12:08:57 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
     <!-- =============================
-                Start: Header Slider
-            ============================= -->
+                    Start: Header Slider
+                ============================= -->
     <div class="tg-sliderholder">
         <div id="tg-homeslider" class="tg-homeslider tg-haslayout" style="padding-bottom: 0!important">
             <div class="pogoSlider-slide" data-transition="fade" data-duration="600"
@@ -55,15 +55,15 @@
     </div>
 
     <!-- =============================
-                End: Header Slider
-            ============================= -->
+                    End: Header Slider
+                ============================= -->
 
 
     @include('layouts.flash')
 
     <!-- =============================
-                Start: About Us
-            ============================= -->
+                    Start: About Us
+                ============================= -->
     <section id="aboutus" class="aboutus section">
         <div class="container">
             <div class="row">
@@ -142,12 +142,12 @@
     </section>
 
     <!-- =============================
-                End: About Us
-            ============================= -->
+                    End: About Us
+                ============================= -->
 
     <!-- =============================
-                Start: service Part
-            ============================= -->
+                    Start: service Part
+                ============================= -->
 
     <section id="service" class="service section">
         <div class="container">
@@ -172,7 +172,8 @@
                                 <i class="flaticon-medical-insurance"></i>
                             </div>
                             <h5>{{ $service->label }} <br><br> <span
-                                    class="badge badge-pill badge-info">{{ $service->price }} XAF</span></h5>
+                                    class="badge badge-pill badge-info">{{ number_format($service->price, 0, ',', ' ') }}
+                                    XAF</span></h5>
                             <p>{{ $service->description }}</p>
                             <div><a class="btn btn-4 btn-ser" href="{{ url('quote/service/' . $service->id) }}">Devis</a>
                             </div>
@@ -206,8 +207,8 @@
             </div>
 
             <!-- =============================
-                        Start: Search
-                    ============================= -->
+                            Start: Search
+                        ============================= -->
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
@@ -223,8 +224,8 @@
                 </div>
             </div>
             <!-- =============================
-                        End: Search
-                    ============================= -->
+                            End: Search
+                        ============================= -->
 
             <br>
             <br>
@@ -241,8 +242,8 @@
             </div>
 
             <!-- =============================
-                        Start: Search
-                    ============================= -->
+                            Start: Search
+                        ============================= -->
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
@@ -260,17 +261,17 @@
                 </div>
             </div>
             <!-- =============================
-                        End: Search
-                    ============================= -->
+                            End: Search
+                        ============================= -->
         </div>
     </section>
     <!-- =============================
-                End: service Part
-            ============================= -->
+                    End: service Part
+                ============================= -->
 
     <!-- =============================
-                Start: Experience Part
-            ============================= -->
+                    Start: Experience Part
+                ============================= -->
 
     <section id="experience" class="experience">
         <div class="container">
@@ -372,11 +373,11 @@
     </section>
 
     <!-- =============================
-                End: Experience Part
-            ============================= -->
+                    End: Experience Part
+                ============================= -->
     <!-- =============================
-                Start: Testimonial Part
-            ============================= -->
+                    Start: Testimonial Part
+                ============================= -->
     <section id="testimonial" class="testimonial section">
         <div class="container">
             <div class="row">
@@ -472,11 +473,11 @@
     </section>
 
     <!-- =============================
-                End: Testimonial Part
-            ============================= -->
+                    End: Testimonial Part
+                ============================= -->
     <!--========================
-             Team Part HTML Start
-            ==========================-->
+                 Team Part HTML Start
+                ==========================-->
     <section id="team" class="team section">
         <div class="container">
             <div class="row">
@@ -519,11 +520,11 @@
     </section>
 
     <!--======================
-             Team Part HTML End
-            =======================-->
+                 Team Part HTML End
+                =======================-->
     <!--======================
-                Latest Updates Part HTML Start
-            =======================-->
+                    Latest Updates Part HTML Start
+                =======================-->
 
     <section id="contactus" class="blog section">
         <div class="container">
@@ -583,7 +584,7 @@
     </section>
 
     <!--======================
-                Blog Part HTML End
-            =======================-->
+                    Blog Part HTML End
+                =======================-->
 
 @endsection
