b256f43246d6eccfa86085b664759190594c6906
Aristide Mebodo <mebodoaristide@yahoo.fr>
2022-08-09 16:41:59 +0100

    Debug Dashboard Admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -187,3 +187,107 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        var chartBar = function() {
+
+            var options = {
+                series: [{
+                    name: 'C.A',
+                    data: [44, 55, 90, 80, 25, 15, 70, 55, 35],
+                    //radius: 12,
+                }],
+                chart: {
+                    type: 'bar',
+                    height: 350,
+
+                    toolbar: {
+                        show: false,
+                    },
+
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: false,
+                        columnWidth: '55%',
+                        endingShape: 'rounded'
+                    },
+                },
+                colors: ['#450b5a'],
+                dataLabels: {
+                    enabled: false,
+                },
+                markers: {
+                    shape: "circle",
+                },
+
+
+                legend: {
+                    show: true,
+                    fontSize: '12px',
+                    labels: {
+                        colors: '#000000',
+
+                    },
+                    markers: {
+                        width: 18,
+                        height: 18,
+                        strokeWidth: 0,
+                        strokeColor: '#fff',
+                        fillColors: undefined,
+                        radius: 12,
+                    }
+                },
+                stroke: {
+                    show: true,
+                    width: 1,
+                    colors: ['transparent']
+                },
+                grid: {
+                    borderColor: '#eee',
+                },
+                xaxis: {
+
+                    categories: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jui', 'Aou', 'Sept'],
+                    labels: {
+                        style: {
+                            colors: '#787878',
+                            fontSize: '13px',
+                            fontFamily: 'poppins',
+                            fontWeight: 100,
+                            cssClass: 'apexcharts-xaxis-label',
+                        },
+                    },
+                    crosshairs: {
+                        show: false,
+                    }
+                },
+                yaxis: {
+                    labels: {
+                        style: {
+                            colors: '#787878',
+                            fontSize: '13px',
+                            fontFamily: 'poppins',
+                            fontWeight: 100,
+                            cssClass: 'apexcharts-xaxis-label',
+                        },
+                    },
+                },
+                fill: {
+                    opacity: 1
+                },
+                tooltip: {
+                    y: {
+                        formatter: function(val) {
+                            return val + " FCFA"
+                        }
+                    }
+                }
+            };
+
+            var chartBar1 = new ApexCharts(document.querySelector("#chartBar"), options);
+            chartBar1.render();
+        }
+    </script>
+@endpush
