287c3a1889302dcf5f483ae2576ba5565655a27e
Aristide Mebodo <mebodoaristide@yahoo.fr>
2022-10-13 23:35:33 +0100

    Dbg 5

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -21,43 +21,45 @@ class HospitalController extends Controller
     public function index()
     {
         $hospitals = Hospital::all();
-        return view('hospitals.list',
-        [
-            'hospitals' => $hospitals,
-        ]);
+        return view(
+            'hospitals.list',
+            [
+                'hospitals' => $hospitals,
+            ]
+        );
     }
 
     public function create(Request $request)
     {
 
-    	$hospital = new Hospital();
+        $hospital = new Hospital();
 
-    	$hospital->label = $request->label;
+        $hospital->label = $request->label;
         $hospital->description = $request->description;
         $hospital->town_id = $request->town_id;
         $country = Town::find($request->town_id);
         $hospital->country_id = $country->country_id;
 
         $picture_1 = FileController::picture($request->file('picture_1'));
-        if($picture_1['state'] == false){
+        if ($picture_1['state'] == false) {
             return back()->with('error', $picture_1['message']);
         }
 
         $hospital->picture_1 = $picture_1['url'];
 
         $picture_2 = FileController::picture($request->file('picture_2'));
-        if($picture_2['state'] == false){
+        if ($picture_2['state'] == false) {
             return back()->with('error', $picture_2['message']);
         }
 
         $hospital->picture_2 = $picture_2['url'];
 
         $hospital->status = STATUT_ENABLE;
-    	$hospital->user_id = auth()->user()->id;
+        $hospital->user_id = auth()->user()->id;
 
-        if($hospital->save()){
+        if ($hospital->save()) {
             return back()->with('success', "L'hôpital a bien été créé !");
-        }else{
+        } else {
             return back()->with('error', 'Une erreur s\'est produite, Veuillez réessayer !');
         }
     }
@@ -65,12 +67,10 @@ class HospitalController extends Controller
     public function edit(Hospital $hospital)
     {
 
-    	if (auth()->user()->id == $hospital->user_id)
-        {
-                return view('edit', compact('hospital'));
-        }
-        else {
-             return redirect('/hospital');
+        if (auth()->user()->id == $hospital->user_id) {
+            return view('edit', compact('hospital'));
+        } else {
+            return redirect('/hospital');
         }
     }
 
@@ -78,45 +78,47 @@ class HospitalController extends Controller
     {
         Controller::he_can('hospitals', 'updat');
 
-        if(isset($_POST['delete'])) {
-    		if($hospital->delete()){
+        if (isset($_POST['delete'])) {
+            if ($hospital->delete()) {
                 return back()->with('success', "L'hôpital a bien été supprimée !");
-            }else{
+            } else {
                 return back()->with('error', "Une erreur s'est produite.");
             }
-    	}else{
+        } else {
             $hospital->label = $request->label;
             $hospital->description = $request->description;
-            $hospital->country_id = $request->country_id;
-            $hospital->town_id = $request->town_id;
 
-            if($request->file('picture_1')){
+            $hospital->town_id = $request->town_id;
+            $town = Town::find($request->town_id);
+            $hospital->country_id = $town->country_id;
+
+            if ($request->file('picture_1')) {
                 $picture_1 = FileController::picture($request->file('picture_1'));
 
-                if($picture_1['state'] == false){
-                    return back()->with('error',$picture_1['message']);
+                if ($picture_1['state'] == false) {
+                    return back()->with('error', $picture_1['message']);
                 }
 
                 $hospital->picture_1 = $picture_1['url'];
             }
 
-            if($request->file('picture_2')){
+            if ($request->file('picture_2')) {
                 $picture_2 = FileController::picture($request->file('picture_2'));
 
-                if($picture_2['state'] == false){
-                    return back()->with('error',$picture_2['message']);
+                if ($picture_2['state'] == false) {
+                    return back()->with('error', $picture_2['message']);
                 }
 
                 $hospital->picture_2 = $picture_2['url'];
             }
 
             $hospital->status = $request->status;
-	    	if($hospital->save()){
+            if ($hospital->save()) {
                 return back()->with('success', "L'hôpital a bien été mis à jour !");
-            }else{
+            } else {
                 return back()->with('error', "Une erreur s'est produite.");
             }
-    	}
+        }
     }
 
     public function sick(Request $request)
@@ -124,8 +126,8 @@ class HospitalController extends Controller
 
         $sicks = Sick::all();
 
-        foreach($sicks as $sick){
-            if($request->get('sick-'.$sick->id) == "on"){
+        foreach ($sicks as $sick) {
+            if ($request->get('sick-' . $sick->id) == "on") {
                 DB::table('hospital_sick')->where([
                     'hospital_id' => $request->get('hospital'),
                     'sick_id' => $sick->id
@@ -137,6 +139,6 @@ class HospitalController extends Controller
             }
         }
 
-        return redirect()->back()->with('success','Maladies affectées !');
+        return redirect()->back()->with('success', 'Maladies affectées !');
     }
 }
