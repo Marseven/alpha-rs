e77f96043f848bc79f5c58f94b2c8b862a0091fd
Aristide Mebodo <mebodoaristide@yahoo.fr>
2022-11-02 13:44:27 +0100

    Debug

--- resources/views/simulator/result.blade.php
+++ resources/views/simulator/result.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--=========================
-                                                                                                                                Breadcrum Part HTML Start
-                                                                                                                                =======================-->
+                                                                                                                                    Breadcrum Part HTML Start
+                                                                                                                                    =======================-->
     <section id="breadcrun" class="breadcrun-banner">
         <div class="container">
             <div class="row">
@@ -82,8 +82,6 @@
                             @if ($simulators->count() == 0)
                                 <tr>
                                     <td><strong>Pas d'élément pour le moment !</strong></td>
-
-
                                 </tr>
                             @endif
                         </tbody>
