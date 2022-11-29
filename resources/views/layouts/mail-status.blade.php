870eabefd8cd0835cdcded8cc3f5875eb68ca290
Aristide Mebodo <mebodoaristide@yahoo.fr>
2022-11-29 23:24:54 +0100

    debug message mail

--- resources/views/layouts/mail-status.blade.php
+++ resources/views/layouts/mail-status.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
     <h1>Hello {{ $data['fullname'] }},</h1>
 
-    Votre {{ $data['entity'] }} a changé de statut. Son nouveau statut est désormais {{ $data['status'] }}.
+    Votre {{ $data['entity'] }} a changé de statut. Son nouveau statut est désormais {{ $data['status']['message'] }}.
 
 
     --
