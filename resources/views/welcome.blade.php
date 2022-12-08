fe6f8ee46372e05364abb2fc9d1dc90298b0ecdc
Aristide Mebodo <mebodoaristide@yahoo.fr>
2022-12-08 12:29:16 +0100

    Debug Texte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -220,8 +220,8 @@
 
 @section('content')
     <!-- =============================
-                                                                                                                                                            Start: Header Slider
-                                                                                                                                                        ============================= -->
+                                                                                                                                                                Start: Header Slider
+                                                                                                                                                            ============================= -->
     <div class="tg-sliderholder">
         <div id="tg-homeslider" class="tg-homeslider tg-haslayout" style="padding-bottom: 0!important">
             <div class="pogoSlider-slide" data-transition="expandReveal" data-duration="1000"
@@ -234,7 +234,7 @@
                                     data-delay="500">Alpha</h6>
                                 <h1 class="pogoSlider-slide-element" data-in="slideUp" data-duration="800" data-delay="500">
                                     <span>Assistance</span>
-                                    Sanitaire
+                                    Évacuation Sanitaire
                                 </h1>
                                 <p>Nous sommes là pour vous car votre santé est importante pous nous.</p>
 
@@ -352,12 +352,12 @@
     </section>
 
     <!-- =============================
-                                                                                                                                                            End: About Us
-                                                                                                                                                        ============================= -->
+                                                                                                                                                                End: About Us
+                                                                                                                                                            ============================= -->
 
     <!-- =============================
-                                                                                                                                                            Start: service Part
-                                                                                                                                                        ============================= -->
+                                                                                                                                                                Start: service Part
+                                                                                                                                                            ============================= -->
 
     <section id="service" class="service section">
         <div class="container">
@@ -500,8 +500,8 @@
             </div>
 
             <!-- =============================
-                                                                                                                                                                    Start: Search
-                                                                                                                                                                ============================= -->
+                                                                                                                                                                        Start: Search
+                                                                                                                                                                    ============================= -->
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
@@ -517,8 +517,8 @@
                 </div>
             </div>
             <!-- =============================
-                                                                                                                                                                    End: Search
-                                                                                                                                                                ============================= -->
+                                                                                                                                                                        End: Search
+                                                                                                                                                                    ============================= -->
 
             <br>
             <br>
@@ -535,8 +535,8 @@
             </div>
 
             <!-- =============================
-                                                                                                                                                                    Start: Search
-                                                                                                                                                                ============================= -->
+                                                                                                                                                                        Start: Search
+                                                                                                                                                                    ============================= -->
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
@@ -554,18 +554,18 @@
                 </div>
             </div>
             <!-- =============================
-                                                                                                                                                                    End: Search
-                                                                                                                                                                ============================= -->
+                                                                                                                                                                        End: Search
+                                                                                                                                                                    ============================= -->
 
         </div>
     </section>
     <!-- =============================
-                                                                                                                                                            End: service Part
-                                                                                                                                                        ============================= -->
+                                                                                                                                                                End: service Part
+                                                                                                                                                            ============================= -->
 
     <!-- =============================
-                                                                                                                                                            Start: Experience Part
-                                                                                                                                                        ============================= -->
+                                                                                                                                                                Start: Experience Part
+                                                                                                                                                            ============================= -->
 
     <section id="experience" class="experience">
         <div class="container">
@@ -672,11 +672,11 @@
     </section>
 
     <!-- =============================
-                                                                                                                                                            End: Experience Part
-                                                                                                                                                        ============================= -->
+                                                                                                                                                                End: Experience Part
+                                                                                                                                                            ============================= -->
     <!-- =============================
-                                                                                                                                                            Start: Testimonial Part
-                                                                                                                                                        ============================= -->
+                                                                                                                                                                Start: Testimonial Part
+                                                                                                                                                            ============================= -->
     <section id="testimonial" class="testimonial section">
         <div class="container">
             <div class="row">
@@ -774,11 +774,11 @@
     </section>
 
     <!-- =============================
-                                                                                                                                                            End: Testimonial Part
-                                                                                                                                                        ============================= -->
+                                                                                                                                                                End: Testimonial Part
+                                                                                                                                                            ============================= -->
     <!--========================
-                                                                                                                                                         Team Part HTML Start
-                                                                                                                                                        ==========================-->
+                                                                                                                                                             Team Part HTML Start
+                                                                                                                                                            ==========================-->
     <section id="team" class="team section">
         <div class="container">
             <div class="row">
@@ -821,11 +821,11 @@
     </section>
 
     <!--======================
-                                                                                                                                                         Team Part HTML End
-                                                                                                                                                        =======================-->
+                                                                                                                                                             Team Part HTML End
+                                                                                                                                                            =======================-->
     <!--======================
-                                                                                                                                                            Latest Updates Part HTML Start
-                                                                                                                                                        =======================-->
+                                                                                                                                                                Latest Updates Part HTML Start
+                                                                                                                                                            =======================-->
 
     <section id="contactus" class="blog section">
         <div class="container">
@@ -885,8 +885,8 @@
     </section>
 
     <!--======================
-                                                                                                                                                            Blog Part HTML End
-                                                                                                                                                        =======================-->
+                                                                                                                                                                Blog Part HTML End
+                                                                                                                                                            =======================-->
 @endsection
 
 @push('scripts')
